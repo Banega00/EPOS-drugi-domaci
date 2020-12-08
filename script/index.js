2a0d6ee3ea7d00162404c9a88ebd278129b59e4b
bane <bane.stojanovic00@gmail.com>
2020-12-08 23:46:48 +0100

    dodat jquery ui - draggable

--- script/index.js
+++ script/index.js
@@ -2,6 +2,19 @@ const ime = document.getElementById("ime");
 const email = document.getElementById("email");
 const text = document.getElementById("pitanje")
 function posaljiPitanje(){
+    if(ime.value=="" || ime.value.length<3){
+        alert('Morate uneti vase ime!');
+        return;
+    }
+    if(email.value=="" || email.value.length<3){
+        alert('Molimo vas unesite ispravnu email adresu!');
+        return;
+    }
+    if(text.value=="" || text.value.length<3){
+        alert('Ne možete poslati prazno pitanje');
+        return;
+    }
+
     ime.value="";
     email.value="";
     text.value="";
